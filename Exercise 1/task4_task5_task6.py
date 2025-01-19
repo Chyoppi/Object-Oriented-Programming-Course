8fdec71fbc60a343f696895eee0b24c55b5fd64e
Chris Männik <chris.mannik@edu.turkuamk.fi>
2025-01-19 20:46:16 +0200

    Task_5 fixed & Task_6

--- Exercise 1/task4_task5_task6.py
+++ Exercise 1/task4_task5_task6.py
@@ -21,13 +21,26 @@ def Negative_integers():
         if x < 0:
             print(x)
 
-#Checks if number is even and if it is prints it.
+#5. Add a function to the previous task that counts the number of even integers (= parilliset luvut in Finnish) that were among the entered
+#Checks if number is even and if it is adds +1 to even value.
 def Even_integers():
-    print("Even numbers:")
+    even_value = 0
     for x in int_list:
         if x % 2 == 0:
-            print(x)
+            even_value += 1
+    print(f"Amount of even numbers: {even_value}")
+
+#6. Add to the previous task a function that counts the sum of the positive integers divisible by three. 
+def Sum_of_three():
+    print("Sum of values which are divisible by 3")
+    sum_three = 0
+    for x in int_list:
+        if x % 3 == 0:
+            if x > 0:
+                sum_three += x
+    print(f"Sum: {sum_three}")
 
 List_loop()
 Negative_integers()
 Even_integers()
+Sum_of_three()
