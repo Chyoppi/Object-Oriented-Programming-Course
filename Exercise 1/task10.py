00a3356df00a3a86874e7404e976897f44924c54
Chyoppi <chris.mannik@edu.turkuamk.fi>
2025-01-20 14:04:41 +0200

    Task_10

--- Exercise 1/task10.py
+++ Exercise 1/task10.py
@@ -22,3 +22,40 @@
     command (1 search, 2 add, 3 quit): 3
     quitting...
 """
+phone_book = {}
+
+def search_phone():
+    name = input("Enter the name: ").strip()
+    if name in phone_book:
+        print(f"{name}'s number is {phone_book[name]}")
+    else:
+        print(f"{name} was not found in the phone book.")
+
+def add_phone():
+    name = input("Enter the name: ").strip()
+    number = input("Enter the phone number: ").strip()
+    if name in phone_book:
+        print(f"{name} already exists. Overwriting the number.")
+    phone_book[name] = number
+    print("Ok!")
+
+def quit():
+    print("Quitting the program. Goodbye!")
+
+def main_loop():
+    while True:
+        user_input = int(input("Command (1 search, 2 add, 3 quit): "))
+        
+        if user_input == 1:
+            search_phone()
+
+        elif user_input == 2:
+            add_phone()
+
+        elif user_input == 3:
+            break
+
+        else:
+            print("Invalid input! Give 1 search, 2 add, 3 quit: ")
+
+main_loop()
