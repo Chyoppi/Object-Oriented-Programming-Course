4cd8813bebee5400e8a902a7d3bef2c5402bd306
Chyoppi <chris.mannik@edu.turkuamk.fi>
2025-02-06 11:52:20 +0200

    task8_exercise2 & modifications task6

--- Exercise 2/task6_ex2.py
+++ Exercise 2/task6_ex2.py
@@ -8,4 +8,6 @@ class Song:
 
 #Printing song details
 song1 = Song("BangerSongs", "Chris", "Rock", 1990)
+song2 = Song("CoolSongs", "Chris", "Rock", 1995)
+song3 = Song("SlowSongs", "Chris", "Rock", 1993)
 print(f"Song title: {song1.title}, artist: {song1.artist}, genre: {song1.genre}, release: {song1.year}")
--- Exercise 2/task8_ex2.py
+++ Exercise 2/task8_ex2.py
@@ -1,3 +1,15 @@
+from task6_ex2 import song1, song2, song3
+
+def songs_of_genre(song_list: list, genre: str):
+    matching_genre = []
+    for song in song_list:
+        if song.genre == genre:
+            matching_genre.append(song)
+    return matching_genre
+
+songs = [song1, song2, song3]
+print("Songs in the rock genre:")
+for song in songs_of_genre(songs, "Rock"):
+    print(f"{song.artist}: {song.title}")
 
 
-def movies_of_genre(movies: list, genre: str)
