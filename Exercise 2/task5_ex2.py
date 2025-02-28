9553b5149b570a571cf2f9793d00a0d6c61fe694
Chyoppi <chris.mannik@edu.turkuamk.fi>
2025-02-28 10:09:09 +0200

    modified

--- Exercise 2/task5_ex2.py
+++ Exercise 2/task5_ex2.py
@@ -11,11 +11,11 @@ def smallest_average(person1: dict, person2: dict, person3: dict):
     avg3 = (person3["result1"] + person3["result2"] + person3["result3"]) / 3
 
     if avg1 < avg2 and avg1 < avg3:
-        return print(f"Smallest average result: {person1["name"]}, with values {person1["result1"]}, {person1["result2"]}, {person1["result3"]}")
+        return print(f'Smallest average result: {person1["name"]}, with values {person1["result1"]}, {person1["result2"]}, {person1["result3"]}')
     elif avg2 < avg1 and avg2 < avg3:
-        return print(f"Smallest average result: {person2["name"]}, with values {person1["result1"]}, {person2["result2"]}, {person2["result3"]}") 
+        return print(f'Smallest average result: {person2["name"]}, with values {person2["result1"]}, {person2["result2"]}, {person2["result3"]}') 
     else:
-        return print(f"Smallest average result: {person3["name"]}, with values {person3["result1"]}, {person3["result2"]}, {person3["result3"]}")
+        return print(f'Smallest average result: {person3["name"]}, with values {person3["result1"]}, {person3["result2"]}, {person3["result3"]}')
     
 
 #Testing the function
